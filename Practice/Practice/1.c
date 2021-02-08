22acb2edcce7326c2ac3e4e9fadf156f16c37fa0
asdf1019 <78126526+asdf1019@users.noreply.github.com>
2021-02-08 17:04:12 +0900

    commit

---
+++ Practice/Practice/1.c
@@ -0,0 +1,20 @@
+#pragma warning(disable : 4996)
+#include <stdio.h>
+
+int main() {
+	int N, sum = 0;
+
+	while (1) {
+		printf("0보다 큰 정수값 N을 입력하세요 : ");
+		scanf("%d", &N);
+		if (N <= 0) {
+			printf("잘못입력하셨습니다. 다시 입력하세요\n");
+		}
+		else break;
+	}
+
+	for (int i = 1; i <= N; i++) {
+		sum += i;
+	}
+	printf("1부터 N까지의 합 : %d", sum);
+}
