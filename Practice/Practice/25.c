fc402e51fb60079bcecb69ada66d213143bb497b
asdf1019 <78126526+asdf1019@users.noreply.github.com>
2021-02-08 19:05:03 +0900

    week2

---
+++ Practice/Practice/25.c
@@ -0,0 +1,15 @@
+/*
+#include <stdio.h>
+void strcpy(char* arr1, char* arr2) {
+	for (int i = 0; i < 10; i++) {
+		arr2[i] = arr1[i];
+	}
+}
+
+void main() {
+	char arr1[] = "hellow";
+	char arr2[10];
+	strcpy(arr1, arr2);
+	printf("%s\n", arr2);
+}
+*/
