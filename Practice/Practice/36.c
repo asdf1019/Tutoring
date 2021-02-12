300d8c9be4ad433b0504874cf75b84c2ca9b3aef
asdf1019 <78126526+asdf1019@users.noreply.github.com>
2021-02-13 06:08:49 +0900

    week3_commit

---
+++ Practice/Practice/36.c
@@ -0,0 +1,10 @@
+/*
+#include <stdio.h>
+void main() {
+	int arr[3] = { 1, 2, 3 };
+	int* ptr;
+
+	ptr = &arr[0];
+
+	printf("%d %d %d", ptr[0], ptr[1], ptr[2]);
+}*/
---
+++ Practice/Practice/38.c
@@ -0,0 +1,9 @@
+/*
+#include <stdio.h>
+void main() {
+	char str[5] = "abcd";
+	char* ptr = "abcd";
+	str[0] = 'x';
+	printf("%s \n", str);
+	printf("%s \n", ptr);
+}*/
