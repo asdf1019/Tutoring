55f481633a4aa887fa725f0f3df3de7407c8509b
asdf1019 <78126526+asdf1019@users.noreply.github.com>
2021-02-13 11:56:55 +0900

    week3_commit

--- Practice/Practice/26.c
+++ Practice/Practice/26.c
@@ -1,15 +1,25 @@
 /*
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
-	int n, (*p)[n], *q;
+	int n;
 	printf("ют╥б: ");
 	scanf("%d", &n);
+	srand((unsigned int)time(NULL));
 
-	p = (int(*)[n])calloc(n * n, sizeof(int));
+	int** p = (int**)malloc(sizeof(int*) * n);
+	for(int i = 0; i < n; i++)
+		p[i] = (int*)malloc(sizeof(int) * n);
 
-
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			p[i][j] = rand() % 100;
+			printf("%d ",p[i][j]);
+		}
+		printf("\n");
+	}
 
 }
 */
